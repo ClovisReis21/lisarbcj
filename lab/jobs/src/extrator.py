0309e00893cb2bde62ba1c6d343db08478226984
Clovis Reis <clovis.fmu@live.com>
2025-04-11 21:40:59 -0300

    extração concluída

--- lab/jobs/src/extrator.py
+++ lab/jobs/src/extrator.py
@@ -1,4 +1,4 @@
-import sys, os, findspark, mysql.connector
+import sys, os, findspark, mysql.connector, shutil
 import pyspark.sql.functions as F
 from src.notificador import Notificador
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, DateType, TimestampType
@@ -9,6 +9,7 @@ class Extrator:
     def __init__(self, sparkSession):
         self.sparkSession = sparkSession
         self.baseDatetime = '2025-03-01 00:00:00.000'
+        self.cleanTime = 30
         self.actors_id = {
             "vendedores": "id_vendedor",
             "vendas": "id_venda",
@@ -27,11 +28,10 @@ class Extrator:
             temp_df = (self.sparkSession.spark.read.parquet(self.pathNewDataMap)
                        .where(F.col("table") == tabela)
                        .sort("date", ascending=False))
-            temp_df.show(truncate=False)
             row = temp_df.collect()[0]
             return f'{row[1]}'
         except Exception as e:
-            self.notificador.Mostrar('info', f'Datetime considerado: {self.baseDatetime} para: "{fullPath}" - {e}')
+            self.notificador.Mostrar('info', f'Datetime considerado "{self.baseDatetime}" para "{fullPath}" - {e}')
             return self.baseDatetime
     
     def SetLastIngestaDatetime(self, tabela, last_date):
@@ -44,8 +44,6 @@ class Extrator:
                 StructField("path", StringType(), False)
             ])
         )
-        newData_df.show(truncate=False)
-        newData_df.printSchema()
         newData_df.write.mode("append").parquet(self.pathNewDataMap)
 
     def GetUserInfo(self, lastDate, tabela): # Read from MySQL procedure
@@ -99,7 +97,6 @@ class Extrator:
 
     def SaveData(self, df, tabela):
         valid_columns = list(set(df.columns) & set(['atualizacao', 'criacao']))
-        print('valid_columns:', valid_columns)
         last_datetime = (df
             .select(valid_columns)
             .withColumn('date', F.coalesce(valid_columns[0], valid_columns[-1]).cast('timestamp'))
@@ -112,27 +109,53 @@ class Extrator:
             self.notificador.Mostrar('info', f'"{strTableDir}" salvo com sucesso!')
             return f'{last_datetime}'
         except Exception as e:
-            self.notificador.Mostrar('error', f'Não foi possível salvar: "{strTableDir}" -> {e}')
+            self.notificador.Mostrar('error', f'Não foi possível salvar "{strTableDir}" -> {e}')
             return self.baseDatetime
+    
+    def DropData(self):
+        newDataMap_keep_df = (
+            self.sparkSession.spark.read.parquet(f'{self.pathNewDataMap}')
+                .where(
+                    (F.to_timestamp(F.col('date')).alias('date') > (F.current_date() - self.cleanTime).alias('date')))
+                .sort('date'))
+
+        newDataMap_delete_df = (
+            self.sparkSession.spark.read.parquet(f'{self.pathNewDataMap}')
+                .where(
+                    (F.to_timestamp(F.col('date')).alias('date') <= (F.current_date() - self.cleanTime).alias('date')))
+                .sort('date'))
+
+        pathsToDelete = newDataMap_delete_df.collect()
+        try:
+            for pathToDelete in pathsToDelete:
+                shutil.rmtree(pathToDelete['path'])
+
+            newDataMap_keep_df.write.mode("overwrite").parquet(f'{self.pathNewDataMap}')
+
+        except Exception as e:
+            self.notificador.Mostrar('info', f'Erro ao limpar MAP TABLE - {e}')
 
     def Run(self):
         tabelas = list(self.actors_id.keys())
         for tabela in tabelas:
-            self.notificador.Mostrar('info', f'Iniciando extração da tabela: "{tabela}"')
+            self.notificador.Mostrar('info', f'Iniciando extração da tabela "{tabela}"')
             last_date = self.GetLastIngestaDatetime(tabela)
             userInfo = self.GetUserInfo(last_date, tabela)
             QTD_LINHAS=userInfo[0][3]
             if QTD_LINHAS == 0:
-                self.notificador.Mostrar('info', f'Não ha novos registros em: {tabela}')
+                self.notificador.Mostrar('info', f'Não ha novos registros em "{tabela}"\n')
                 continue
             MAX_CONN=userInfo[0][0]
             LOW_BOUND=userInfo[0][1]
             UPPER_BOUND=userInfo[0][2]
             df = self.GetData(MAX_CONN, LOW_BOUND, UPPER_BOUND, last_date, tabela)
             if df == None:
-                self.notificador.Mostrar('error', f'Registros não encontrados em: {tabela}')
+                self.notificador.Mostrar('error', f'Registros não encontrados em "{tabela}"\n')
                 continue
-            df.show(1)
             last_date = self.SaveData(df, tabela)
             self.SetLastIngestaDatetime(tabela, last_date)
-            self.notificador.Mostrar('info', f'Extração da tabela: "{tabela}" concluída!\n')
+            self.notificador.Mostrar('info', f'Extração da tabela "{tabela}" concluída!\n')
+        baseNewDataMap = self.pathNewDataMap.split("/")[-1]
+        self.notificador.Mostrar('info', f'Iniciando atualização da base "{baseNewDataMap}"!')
+        self.DropData()
+        self.notificador.Mostrar('info', f'Base "{baseNewDataMap}" atualizada!\n')
