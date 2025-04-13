5f65edc7acdcc4ccfe0519b9850ef8038565d481
Clovis Reis <clovis.fmu@live.com>
2025-04-13 16:09:16 -0300

    batch view completo

--- lab/jobs/main.py
+++ lab/jobs/main.py
@@ -2,6 +2,7 @@ import sys
 from src.spark import Spark
 from src.extrator import Extrator
 from src.ingesta import Ingesta
+from src.batchViews import BatchViews
 
 sparkSession = Spark()
 
@@ -9,3 +10,5 @@ if sys.argv[1] == 'extrator':
     Extrator(sparkSession).Run()
 elif sys.argv[1] == 'ingesta':
     Ingesta(sparkSession).Run()
+elif sys.argv[1] == 'batch':
+    BatchViews(sparkSession).Run()
